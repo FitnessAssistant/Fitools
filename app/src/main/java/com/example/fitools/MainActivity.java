55e9d268c2c12ed909d5cdc58c0dfee634544639
申玥 <1013250411@qq.com>
2018-01-30 15:59:04 +0800

    第一次合并实验

--- app/src/main/java/com/example/fitools/MainActivity.java
+++ app/src/main/java/com/example/fitools/MainActivity.java
@@ -6,10 +6,14 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //母项目第一次提交,实验团队合作--Santa
+
+
+    //我写了一大段代码--玥玥
     //第二次实验-Santa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //我又写了一大段代码-玥玥
         setContentView(R.layout.activity_main);
     }
 }
