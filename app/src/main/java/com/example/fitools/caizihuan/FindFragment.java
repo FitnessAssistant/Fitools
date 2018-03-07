5ddc7901b3e0f023abb1b95978130333fed93994
caizihuan <2511480600@qq.com>
2018-03-07 20:06:37 +0800

    4个fragment

---
+++ app/src/main/java/com/example/fitools/caizihuan/FindFragment.java
@@ -0,0 +1,30 @@
+package com.example.fitools.caizihuan;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.fitools.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FindFragment extends Fragment {
+
+
+    public FindFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.czh_fragment_find, container, false);
+    }
+
+}
