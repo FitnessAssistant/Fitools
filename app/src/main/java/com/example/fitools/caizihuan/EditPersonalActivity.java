7e5d68304bcbe7d6f2610bf31e7cfe00f6fc7887
caizihuan <2511480600@qq.com>
2018-05-11 16:38:30 +0800

    编辑个人信息

---
+++ app/src/main/java/com/example/fitools/caizihuan/EditPersonalActivity.java
@@ -0,0 +1,15 @@
+package com.example.fitools.caizihuan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.fitools.R;
+
+public class EditPersonalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_personal);
+    }
+}
